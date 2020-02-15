03a3b81314ddd38630cb1258ea5b2aeb0e2cb3a4
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-02-15 00:00:23 -0800

    Updates

---
+++ precache-manifest.84450f17d28531abfe606b3ff91d2034.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "872b8b3a8501d598eb4453841689f778",
+    "url": "/neartalk/index.html"
+  },
+  {
+    "revision": "e806705612b726c0ef27",
+    "url": "/neartalk/static/css/2.5c17b9f1.chunk.css"
+  },
+  {
+    "revision": "876986b861ef1c1fbfc7",
+    "url": "/neartalk/static/css/main.7f542885.chunk.css"
+  },
+  {
+    "revision": "e806705612b726c0ef27",
+    "url": "/neartalk/static/js/2.4f0acbb3.chunk.js"
+  },
+  {
+    "revision": "b77605629a06e8ef2b89aaeeacddcdf7",
+    "url": "/neartalk/static/js/2.4f0acbb3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "876986b861ef1c1fbfc7",
+    "url": "/neartalk/static/js/main.22019aed.chunk.js"
+  },
+  {
+    "revision": "4645bf177274931f7e04",
+    "url": "/neartalk/static/js/runtime-main.1f1403a4.js"
+  },
+  {
+    "revision": "5cbbd24358daf76269f11c28186080b8",
+    "url": "/neartalk/static/media/anon.5cbbd243.png"
+  },
+  {
+    "revision": "475ce496c2bb8d01c33abb54f3ff05be",
+    "url": "/neartalk/static/media/encryptionOff.475ce496.png"
+  },
+  {
+    "revision": "23e3b306fa3c3b6857296e3574444bdc",
+    "url": "/neartalk/static/media/encryptionOn.23e3b306.png"
+  },
+  {
+    "revision": "55517b93c50499eda5108ca08447bb25",
+    "url": "/neartalk/static/media/gray_near_logo.55517b93.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c53f0c04d7098ee3dbbb5a34545c9a63.js"
+  "/neartalk/precache-manifest.84450f17d28531abfe606b3ff91d2034.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/neartalk/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
