0af9b11b09e085690a62d7c3742cadd451316081
Benjamin Huh <benjamin.s.huh@gmail.com>
2020-02-16 22:22:41 -0800

    Add contract

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 (function() {
-    const CONTRACT_NAME = 'neardev'; /* TODO: fill this in!*/
+    const CONTRACT_NAME = 'bhdev'; /* TODO: fill this in!*/
     const DEFAULT_ENV = 'development'; 
     
     function getConfig(env) {
